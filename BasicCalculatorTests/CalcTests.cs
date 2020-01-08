17d8fb04f1cf29019dd0c5d7f4032388b6d4a028
gmachalo <57180690+gmachalo@users.noreply.github.com>
2020-01-08 10:42:23 +0200

    Task: Added Basic Calculator Project

---
+++ BasicCalculatorTests/CalcTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BasicCalculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCalculator.Tests
+{
+    [TestClass()]
+    public class CalcTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Calc calculator = new Calc();
+
+            int c = calculator.Add(1, 2);
+
+            Assert.AreEqual(3, c);
+            Assert.AreEqual(3, calculator.result);
+
+        }
+        [TestMethod()]
+        public void DivTest()
+        {
+            Calc calculator = new Calc();
+
+            int c = calculator.Div(2, 1);
+
+            Assert.AreEqual(2, c);
+            Assert.AreEqual(2, calculator.result);
+
+        }
+    }
+}
---
+++ operationsTests/SquareRootTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using operations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace operations.Tests
+{
+    [TestClass()]
+    public class SquareRootTests
+    {
+        private readonly double a = 25.0;
+
+        [TestMethod()]
+        public void RootTest()
+        {
+            Assert.AreEqual(5, SquareRoot.Root(a));
+        }
+    }
+}
